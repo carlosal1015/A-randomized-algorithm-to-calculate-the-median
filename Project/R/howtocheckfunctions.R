c3af0867a1808ef0ca88015eb5d2e070e7c977b7
carlosal1015 <caznaranl@uni.pe>
2018-09-18 19:29:50 -0500

    Adding R code how to show syntax's function

---
+++ Project/R/howtocheckfunctions.R
@@ -0,0 +1,9 @@
+setwd("~/GitHub/A-randomized-algorithm-to-calculate-the-median/Project/R")
+print("Primero escribimos el nombre de la función de R sin paréntesis\nVeamos para la función t de Student")
+t.test
+methods(t.test)
+getAnywhere(t.test.default)
+print("Ahora para la función media:")
+median
+methods(median)
+getAnywhere(median.default)
