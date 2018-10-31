3640bf8379923cc57becc5d5c60beb89c22b16ca
Oromion <caznaranl@uni.pe>
2018-10-31 00:08:11 -0500

    Update

---
+++ Project/Members/Carlos/Debugging_C_Code/Debugging_C_Code/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Debugging_C_Code
+//
+//  Created by Oromion on 10/26/18.
+//  Copyright © 2018 Oromion. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
